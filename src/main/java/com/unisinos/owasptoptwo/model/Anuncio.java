09ebbef4f94070a228b509defa3f4edf9fcbf6cb
Lucas Cerveira <lucas.tempass@hotmail.com>
2024-03-11 20:56:43 -0300

    ✨ Cria entidades

---
+++ src/main/java/com/unisinos/owasptoptwo/model/Anuncio.java
@@ -0,0 +1,25 @@
+package com.unisinos.owasptoptwo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Anuncio {
+
+	@Id
+	private Integer id;
+
+	@Column(name = "titulo", nullable = false, length = 128)
+	private String titulo;
+
+	@Column(name = "descricao", nullable = false, length = 1024)
+	private String descricao;
+
+	@ManyToOne
+	@JoinColumn(name = "dono_id", nullable = false)
+	private Usuario dono;
+
+}
---
+++ src/main/java/com/unisinos/owasptoptwo/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.unisinos.owasptoptwo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+
+	@Id
+	private String id;
+
+	@Column(name = "nome", nullable = false, length = 128)
+	private String nome;
+
+	@Column(name = "email", nullable = false, length = 256)
+	private String email;
+
+	@Column(name = "senha", nullable = false, length = 64)
+	private String senha;
+
+	@OneToMany(mappedBy = "dono", orphanRemoval = true, fetch = FetchType.LAZY)
+	private List<Anuncio> anuncios;
+
+}
