37aa3b76619187127a53a7a12693a45b80eaba68
Lucas Cerveira <lucas.tempass@hotmail.com>
2024-03-11 20:40:47 -0300

    🎉 Inicia projeto

---
+++ src/test/java/com/unisinos/owasptoptwo/OwaspTopTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisinos.owasptoptwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwaspTopTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
