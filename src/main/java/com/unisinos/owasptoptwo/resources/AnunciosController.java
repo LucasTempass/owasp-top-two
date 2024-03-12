0c0487269e42c9d0ae2887d071d71c21517b99cb
Lucas Cerveira <lucas.tempass@hotmail.com>
2024-03-11 21:29:17 -0300

    💡 Adiciona comentários sobre CWE

--- src/main/java/com/unisinos/owasptoptwo/resources/AnunciosController.java
+++ src/main/java/com/unisinos/owasptoptwo/resources/AnunciosController.java
@@ -15,24 +15,30 @@ public class AnunciosController {
 
 	@GetMapping("/{id}")
 	public Anuncio getAnuncio(@PathVariable Integer id) {
+		// CWE-200: Exposure of Sensitive Information to an Unauthorized Actor
+		// Ao retornar a Entidade Anuncio diretamente, estamos expondo informações sensíveis sobre o anúncio e seu dono.
 		return anuncioService.findById(id).orElseThrow(() ->
 				new ResponseStatusException(HttpStatusCode.valueOf(404), "Não foi possível encontraro  anúncio.")
 		);
 	}
 
-	@PostMapping
-	public Integer createAnuncio(@RequestBody Anuncio anuncio) {
-		return anuncioService.create(anuncio);
-	}
-
 	@PutMapping("/{id}")
 	public void updateAnuncio(@PathVariable Integer id, @RequestBody Anuncio anuncio) {
+		// CWE-862: Missing Authorization
+		// Não estamos verificando se o usuário que está tentando atualizar o anúncio é o dono do anúncio.
 		anuncioService.update(id, anuncio);
 	}
 
 	@DeleteMapping("/{id}")
 	public void deleteAnuncio(@PathVariable Integer id) {
+		// CWE-862: Missing Authorization
+		// Não estamos verificando se o usuário que está tentando deletar o anúncio é o dono do anúncio.
 		anuncioService.delete(id);
+	}
+
+	@PostMapping
+	public Integer createAnuncio(@RequestBody Anuncio anuncio) {
+		return anuncioService.create(anuncio);
 	}
 
 }
